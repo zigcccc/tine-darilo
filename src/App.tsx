372856f8268937a52a5b76762464618dffb44bd0
Žiga Krašovec <zkrasovec@gmail.com>
2025-05-31 22:01:22 +0200

    feat: added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { MediaPlayer, type MediaPlayerInstance, MediaProvider, Poster } from '@vidstack/react';
-import { useRef } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
+import { VideoPuzzle } from './components';
 
 export function App() {
-  const ref = useRef<MediaPlayerInstance>(null);
-
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <MediaPlayer
-        ref={ref}
-        controls={true}
-        fullscreenOrientation="portrait"
-        load="eager"
-        src="/test.mov"
-        title="Sprite Fight"
-      >
-        <MediaProvider>
-          <Poster
-            alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
-            className="absolute inset-0 block h-full w-full rounded-md bg-black opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
-            src="https://files.vidstack.io/sprite-fight/poster.webp"
-          />
-        </MediaProvider>
-      </MediaPlayer>
-    </div>
+    <>
+      <VideoPuzzle />
+      <Analytics />
+    </>
   );
 }
---
+++ src/components/VideoPuzzle/VideoPuzzle.tsx
@@ -0,0 +1,27 @@
+import { MediaPlayer, type MediaPlayerInstance, MediaProvider, Poster } from '@vidstack/react';
+import { useRef } from 'react';
+
+export function VideoPuzzle() {
+  const ref = useRef<MediaPlayerInstance>(null);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <MediaPlayer
+        ref={ref}
+        controls={true}
+        fullscreenOrientation="portrait"
+        load="eager"
+        src="/test.mov"
+        title="Sprite Fight"
+      >
+        <MediaProvider>
+          <Poster
+            alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+            className="absolute inset-0 block h-full w-full rounded-md bg-black opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
+            src="https://files.vidstack.io/sprite-fight/poster.webp"
+          />
+        </MediaProvider>
+      </MediaPlayer>
+    </div>
+  );
+}
